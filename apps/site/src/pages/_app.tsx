11c760befdd2d8beaf2e7bae5fc394295582d3bb
Shivam Pal <shibru127@gmail.com>
2024-08-24 04:02:53 +0530

    add opengraph meta

--- apps/site/src/pages/_app.tsx
+++ apps/site/src/pages/_app.tsx
@@ -1,6 +1,19 @@
 import "@/styles/globals.css";
 import type { AppProps } from "next/app";
+import Head from "next/head";
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  return (
+    <>
+      <Head>
+        <meta property="og:title" content="Resocket" />
+        <meta
+          property="og:description"
+          content="Devtools for realtime and multiplayer apps."
+        />
+        <meta property="og:url" content="https://resocket.io" />
+      </Head>
+      <Component {...pageProps} />
+    </>
+  );
 }
