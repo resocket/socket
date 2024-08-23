c47f37ef4ad36e48d2e6bc9c4c2bf41b5b75afa0
Shivam Pal <shibru127@gmail.com>
2024-08-24 02:13:39 +0530

    finalizing docs

--- apps/docs/theme.config.jsx
+++ apps/docs/theme.config.jsx
@@ -1,7 +1,22 @@
 import { useRouter } from "next/router";
+import { useConfig } from "nextra-theme-docs";
 
 export default {
   logo: "Resocket",
+  project: {
+    link: "https://github.com/resocket/socket",
+  },
+  chat: {
+    link: "https://discord.gg/FQb86Sqxhd",
+  },
+  footer: {
+    text: (
+      <span>
+        Socket is an open-source project of{" "}
+        <a href="https://resocket.io">Resocket.</a>
+      </span>
+    ),
+  },
   useNextSeoProps() {
     const { asPath } = useRouter();
     if (asPath !== "/") {
@@ -14,7 +29,31 @@ export default {
       };
     }
   },
+  head: () => {
+    const { frontMatter } = useConfig();
+
+    return (
+      <>
+        <meta
+          property="og:description"
+          content={
+            frontMatter.description ||
+            "Devtools for realtime and multiplayer apps."
+          }
+        />
+        <meta name="twitter:card" content="summary_large_image" />
+        <meta name="twitter:site" content="@resocket_io" />
+        <meta name="twitter:title" content="Resocket" />
+        <meta
+          name="twitter:description"
+          content="Devtools for realtime and multiplayer apps."
+        />
+      </>
+    );
+  },
   nextThemes: {
     defaultTheme: "dark",
   },
+  docsRepositoryBase:
+    "https://github.com/resocket/socket/tree/master/apps/docs",
 };
